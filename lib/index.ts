9659cdfca2a1ca81137cda0bd7d5c7c4189b9b22
Hais Rodolphe <44665224+HaisRodolphe@users.noreply.github.com>
2020-01-31 15:30:02 +0100

    Mise en place "canvas KIRBY"

--- lib/index.ts
+++ lib/index.ts
@@ -1,2 +1,37 @@
-import {hello} from "./hello";
-hello();
+function init() {
+    let canvas = getCanvas();
+    let context = getContext();
+
+// on détermine les dimensions du canvas
+canvas.width = 640;
+canvas.height = 480;
+// configuration KIRBY
+// sauvegarder le contexte =t1
+context.save();
+
+// crée un nouveau path
+context.beginPath();
+
+// on restaure le dernier contexte sauvegardé avec save();
+context.restore();
+
+// Fin configuration KIRBY
+/*
+ * Retourne une référence à l'objet canvas crée à l'aide de la balise
+ * placée dans le code html
+ */
+function getContext() {
+    return getCanvas().getContext("2d")
+}
+/*
+ * Retourne une référence à l'objet canvas crée à l'aide de la balise
+ * placée dans le code html
+ */
+function getCanvas() {
+    return document.querySelector("canvas");
+}
+/*
+ * Quand toutes les données sont chargées (DOM, images, sons etc...)
+ * On démarre l'application par la fonction init
+ */
+window.addEventListener("load", init);
